0654709f3289649aff18bea0d1ec3c9ea475a106
ljp2 <ljp2@lehigh.edu>
2022-09-22 20:47:58 -0400

    working

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
 import iso8601
+import pytz
+from datetime import datetime
+
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest, GetOrdersRequest, GetOrderByIdRequest
 from alpaca.trading.enums import OrderSide, OrderStatus, TimeInForce
@@ -8,20 +11,29 @@ secretkey = "hBnEAEiD1f67p6hM4DKkUBtixY01YulNWSuGHOyx"
 
 trading_client = TradingClient(apikey, secretkey, paper=True)
 
+
 def get_clock():
     global trading_client
     clock = trading_client.get("/clock")
     return clock
 
-def get_datetime_aware():
+
+def get_datetime_aware() -> datetime:
     clock = get_clock()
     timestamp = clock["timestamp"]
     dt = iso8601.parse_date(timestamp)
     return dt
 
 
-dt = get_datetime_aware()
-print(dt)
-print(dt.tzinfo)
-print(dt.tzname())
-print(dt.isoformat())
+def get_datetime_tz(timezone) -> datetime:
+    dt = get_datetime_aware()
+    return dt.astimezone(timezone)
+
+
+def get_datetime_eastern() -> datetime:
+    return get_datetime_tz(pytz.timezone("US/Eastern"))
+
+
+def get_datetime_utc() -> datetime:
+    return get_datetime_tz(pytz.timezone("UTC"))
+
