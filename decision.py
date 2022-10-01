e9e1e104c18cce3d2c8c91fd01a39838bfdebb8b
ljp2 <ljp2@lehigh.edu>
2022-09-30 21:10:45 -0400

    working

--- decision.py
+++ decision.py
@@ -4,71 +4,73 @@ from position import get_position, set_position
 
 
 def decide(all_bars: Bars):
-    pos = position.get_position()
     habar = all_bars.last_habar
     c = habar.color
     match c:
         case 'GREEN':
-            process_green(all_bars, pos)
+            process_green(all_bars)
         case 'RED':
-            process_red(all_bars, pos)
+            process_red(all_bars)
         case 'YELLOW':
-            process_yellow(all_bars, pos)
+            process_yellow(all_bars)
         case _:
             raise Exception('Color Problem', 'c =', c)
 
 
-def process_green(all_bars, pos):
+def process_green(all_bars):
     print("Processing GREEN")
-    if pos < 0:
+    pos = get_position()
+    if pos["quantity"] < 0:
         close_short_go_long(all_bars, pos)
-    elif pos == 0:
+    elif pos["quantity"] == 0:
         go_long(all_bars, pos)
-    elif pos > 0:
+    elif pos["quantity"] > 0:
         # do nothing, stay long
         pass
     else:
         raise Exception("Position value problem")
 
 
-def process_red(all_bars, pos):
+def process_red(all_bars):
     print("Processing RED")
-    if pos < 0:
+    pos = get_position()
+    if pos["quantity"] < 0:
         # do nothing, stay short
         pass
-    elif pos == 0:
+    elif pos["quantity"] == 0:
         go_short(all_bars, pos)
-    elif pos > 0:
+    elif pos["quantity"] > 0:
         close_long_go_short(all_bars, pos)
     else:
         raise Exception("Position value problem")
 
 
-def process_yellow(all_bars, pos):
+def process_yellow(all_bars):
     print("Processing YELLOW")
-    if pos < 0:
+    pos = get_position()
+    if pos["quantity"] < 0:
         close_short(all_bars, pos)
-    elif pos == 0:
+    elif pos["quantity"] == 0:
         # stay at zero position
         pass
-    elif pos > 0:
+    elif pos["quantity"] > 0:
         close_long(all_bars, pos)
     else:
         raise Exception("Position value problem")
 
 
-def close_all_positions(all_bars, pos):
-    print("Closing all positions")
-    position.set_position(0)
+# def close_all_positions(all_bars, pos):
+#     print("Closing all positions")
+#     position.set_position(0)
 
 
 def close_long_go_short(all_bars, pos):
-    close_all_positions(all_bars, pos)
+    close_long(all_bars, pos)
     go_short(all_bars, pos)
 
 
 def close_short_go_long(all_bars, pos):
-    close_all_positions(all_bars, pos)
+    close_short(all_bars, pos)
     go_long(all_bars, pos)
 
 
--- position.py
+++ position.py
@@ -1,10 +1,18 @@
-__position: int = 0
+__position = {
+    "quantity": 0,
+    "value": 0.0,
+    "profit_loss": 0.0
+}
 
 def get_position():
     global __position
     return __position
 
-def set_position(value: int):
+def set_position(quantity: int, value: float):
     global __position
-    __position = value
+    __position["quantity"] = quantity
+    __position["value"] = value
     return __position
+
+def update_profit_loss(value:float):
+    __position['profit_loss'] += value
