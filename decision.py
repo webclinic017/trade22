03c7bb71894be66a4af0be7866be71eb281100e7
ljp2 <ljp2@lehigh.edu>
2022-10-02 15:14:42 -0400

    working

--- decision.py
+++ decision.py
@@ -1,60 +1,61 @@
 import position
 from bars import HAbar, Bar, Bars
-from position import get_position, set_position, get_value,\
-    get_quantity, get_profit_loss, get_quantity_value
+from position import get_position, set_quantity, set_price, set_quantity_price, \
+    add_to_profit_loss
 
 
 def decide(all_bars: Bars):
     habar = all_bars.last_habar
     c = habar.color
+    pos = get_position()
     match c:
         case 'GREEN':
-            process_green(all_bars)
+            process_green(all_bars, pos)
         case 'RED':
-            process_red(all_bars)
+            process_red(all_bars, pos)
         case 'YELLOW':
-            process_yellow(all_bars)
+            process_yellow(all_bars, pos)
         case _:
             raise Exception('Color Problem', 'c =', c)
 
 
-def process_green(all_bars):
+def process_green(all_bars, pos):
     print("Processing GREEN")
-    pos = get_position()
-    if pos["quantity"] < 0:
+    quantity = pos["quantity"]
+    if quantity < 0:
         close_short_open_long_position(all_bars, pos)
-    elif pos["quantity"] == 0:
+    elif quantity == 0:
         open_long_position(all_bars, pos)
-    elif pos["quantity"] > 0:
+    elif quantity > 0:
         # do nothing, stay long
         pass
     else:
         raise Exception("Position value problem")
 
 
-def process_red(all_bars):
+def process_red(all_bars, pos):
     print("Processing RED")
-    pos = get_position()
-    if pos["quantity"] < 0:
+    quantity = pos["quantity"]
+    if quantity < 0:
         # do nothing, stay short
         pass
-    elif pos["quantity"] == 0:
+    elif quantity == 0:
         open_short_position(all_bars, pos)
-    elif pos["quantity"] > 0:
+    elif quantity > 0:
         close_long_open_short_position(all_bars, pos)
     else:
         raise Exception("Position value problem")
 
 
-def process_yellow(all_bars):
+def process_yellow(all_bars, pos):
     print("Processing YELLOW")
-    pos = get_position()
-    if pos["quantity"] < 0:
+    quantity = pos["quantity"]
+    if quantity < 0:
         close_short(all_bars, pos)
-    elif pos["quantity"] == 0:
+    elif quantity == 0:
         # stay at zero position
         pass
-    elif pos["quantity"] > 0:
+    elif quantity > 0:
         close_long(all_bars, pos)
     else:
         raise Exception("Position value problem")
@@ -65,6 +66,38 @@ def process_yellow(all_bars):
 #     position.set_position(0)
 
 
+def open_long_position(all_bars, pos):
+    print("Going Long")
+    price = all_bars.last_raw_bar.close
+    quantity = 100
+    set_quantity_price(quantity, price)
+
+
+def open_short_position(all_bars, pos):
+    print("Going Short")
+    price = all_bars.last_raw_bar.close
+    quantity = -100
+    set_quantity_price(quantity, price)
+
+
+def close_long(all_bars, pos):
+    print("Closing Long")
+    current_price = all_bars.last_raw_bar.close
+    quantity = pos["quantity"]
+    price = pos["price"]
+    pl = quantity * (current_price - price)
+    add_to_profit_loss(pl)
+
+
+def close_short(all_bars, pos):
+    print("Closing Short")
+    current_price = all_bars.last_raw_bar.close
+    quantity = pos["quantity"]
+    price = pos["price"]
+    pl = quantity * (price - current_price)
+    add_to_profit_loss(pl)
+
+
 def close_long_open_short_position(all_bars, pos):
     close_long(all_bars, pos)
     open_short_position(all_bars, pos)
@@ -73,28 +106,3 @@ def close_long_open_short_position(all_bars, pos):
 def close_short_open_long_position(all_bars, pos):
     close_short(all_bars, pos)
     open_long_position(all_bars, pos)
-
-
-def open_long_position(all_bars, pos):
-    print("Going Long")
-    price = all_bars.last_raw_bar.close
-
-    set_position(100)
-
-
-def open_short_position(all_bars, pos):
-    price = all_bars.last_raw_bar.close
-    print("Going Short")
-    set_position(-100)
-
-
-def close_long(all_bars, pos):
-    print("Closing Long")
-    set_position(0)
-
-
-def close_short(all_bars, pos):
-    print("Closing Short")
-    current_price = all_bars.last_raw_bar.close
-    quantity, value = get_quantity_value()
-
--- position.py
+++ position.py
@@ -10,17 +10,6 @@ def get_position():
     return __position
 
 
-def set_position(quantity: int, price: float):
-    global __position
-    __position["quantity"] = quantity
-    __position["price"] = price
-    return __position
-
-
-def update_profit_loss(value: float):
-    __position['profit_loss'] += value
-
-
 def get_quantity():
     return __position["quantity"]
 
@@ -29,9 +18,29 @@ def get_price():
     return __position["price"]
 
 
-def get_quantity_value():
-    return __position["value"], __position["quantity"]
+def get_quantity_price():
+    return __position["quantity"], __position["price"]
 
 
 def get_profit_loss():
     return __position["profit_loss"]
+
+
+def set_quantity(quantity: int):
+    global __position
+    __position["quantity"] = quantity
+
+
+def set_price(price: float):
+    global __position
+    __position.price = price
+
+
+def set_quantity_price(quantity: int, price: float):
+    global __position
+    __position["quantity"] = quantity
+    __position["price"] = price
+
+
+def add_to_profit_loss(value: float):
+    __position['profit_loss'] += value
